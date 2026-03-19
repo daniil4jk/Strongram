0a0d2869602cb2290fcd13d047f925b2b63cf0e5
daniil4jk <daniilprotop@bk.ru>
2026-03-19 13:18:16 +0500

    add log to executor

--- strongram-core/src/main/java/ru/daniil4jk/strongram/core/util/DefaultExecutor.java
+++ strongram-core/src/main/java/ru/daniil4jk/strongram/core/util/DefaultExecutor.java
@@ -1,23 +1,29 @@
 package ru.daniil4jk.strongram.core.util;
 
+import lombok.extern.slf4j.Slf4j;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
+@Slf4j
 public class DefaultExecutor {
     private static final Lazy<ScheduledExecutorService> DEFAULT_EXECUTOR = new Lazy<>(
             DefaultExecutor::createScheduledCashedThreadPool
     );
 
     private static @NotNull ScheduledThreadPoolExecutor createScheduledCashedThreadPool() {
-        var exec = new ScheduledThreadPoolExecutor(1);
+        var exec = new ScheduledThreadPoolExecutor(Runtime.getRuntime().availableProcessors());
         exec.setKeepAliveTime(60, TimeUnit.SECONDS);
         return exec;
     }
 
     public static ScheduledExecutorService initOrGet() {
+        if (!isInitialized()) {
+            log.info("Creating ru.daniil4jk.strongram.core.util.DefaultExecutor thread pool. " +
+                    "It is used when no explicit Executor is specified in some of Strongram components");
+        }
         return DEFAULT_EXECUTOR.initOrGet();
     }
 
